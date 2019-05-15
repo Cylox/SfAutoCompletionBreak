7303ec1a6ed87d6e419f0ef621200e8ebdd5f519
Cylox <cygo.cp@gmail.com>
2019-05-15 10:46:12 +0200

    Add minimum working example for auto completion problem with backslashes

---
+++ SfAutoCompletionBreak/AutoCompletionBreak.feature
@@ -0,0 +1,7 @@
+﻿Feature: AutoCompletionBreak
+		When using an escaped parenthesis in a specflow step attribute regex like this: '\(' the auto completion
+		only will insert the step text up to the first backslash
+
+
+Scenario: Try to insert step text though auto completion
+	Given I have a step with coordinates like this: \{x}
--- SfAutoCompletionBreak/Class1.cs
+++ SfAutoCompletionBreak/Class1.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechTalk.SpecFlow;
 
 namespace SfAutoCompletionBreak
 {
+
+    [Binding]
     public class Class1
     {
+        [Given(@"I have a step with coordinates like this: \(([\d\.\,]+|x),\s?([\d\.\,]+|y)\)")]
+        public void PrintCoordinates(double x, double y)
+        {
+            Console.WriteLine($"The coordinates are: x={x}, y={y}");
+        }
     }
 }
